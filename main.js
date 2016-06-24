30d164137321249ca8ded4487fa032366bc40355
David Smith <dbsmith.dbs83@gmail.com>
2016-06-23 18:56:01 -0700

    building out files, content

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var myForm = document.getElementById('myForm');
 var buyLink = document.getElementById('buy');
 
 function openOrderForm(event) {
-  alert('buy some stuff, dude');
+  alert('buy clicked');
+  window.open('orderform.html');
 }
 
 buyLink.addEventListener('click', openOrderForm, false);
