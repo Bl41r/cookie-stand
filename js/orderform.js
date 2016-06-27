cf9f0a3bc571cea3027a202abe2a624be4d23302
David Smith <dbsmith.dbs83@gmail.com>
2016-06-26 21:10:43 -0700

    orderform sends data to array

--- js/orderform.js
+++ js/orderform.js
@@ -11,20 +11,25 @@ function newOrder() {
   order[2] = document.getElementById('city').value;
   order[3] = document.getElementById('zip').value;
   order[4] = document.getElementById('cardtype').value;
-  order[5] = document.getElementById('number').value;
+  order[5] = document.getElementById('cc').value;
   order[6] = document.getElementById('exp').value;
+  order[7] = document.getElementById('q').value;
   if (confirm('Are you sure you want to make this purchase?')) {
     pendingOrders.push(order);
     alert('Order received!');
-    document.getElementById('name').value = '';
-    document.getElementById('addy').value = '';
-    document.getElementById('city').value = '';
-    document.getElementById('zip').value = '';
-    document.getElementById('cardtype').value = 'visa';
-    document.getElementById('number').value = '';
-    document.getElementById('exp').value = '';
-
+    clearOrders();
   }
+}
+
+function clearOrders() {
+  document.getElementById('name').value = '';
+  document.getElementById('addy').value = '';
+  document.getElementById('city').value = '';
+  document.getElementById('zip').value = '';
+  document.getElementById('cardtype').value = 'visa';
+  document.getElementById('cc').value = '';
+  document.getElementById('exp').value = '';
+  document.getElementById('q').value = '';
 }
 
 orderForm.addEventListener('submit', newOrder, false);
